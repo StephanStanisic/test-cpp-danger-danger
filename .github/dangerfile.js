3009bbd94dc0b1827bca9ed0db9a6e8f4ded137e
Stephan Stanisic <stephan@stanisic.nl>
2022-04-20 22:50:56 +0200

    dfhjgsdf

--- .github/dangerfile.js
+++ .github/dangerfile.js
@@ -1,3 +1,13 @@
 import { message, warn, markdown, danger } from 'danger'
 
+// Check if branch name is correct
+let branch_parts = process.env.BRANCH.split("/");
+let pull_request_id = branch_parts.shift();
+let allowed_branches = ["feature", "release", "hotfix", "refactor"];
+if(!allowed_branches.includes(branch_parts[0]))
+	fail(`Branch name is incorrect! \`${branch_parts[0]}\` is not a valid branch prefix! Pick one of ${allowed_branches.join(', ')}.`);
+
+// Check if docs are updated
+console.log(danger.git.modified_files, danger.git.created_files);
+
 message(`Eyo ${process.env.BRANCH}`)
